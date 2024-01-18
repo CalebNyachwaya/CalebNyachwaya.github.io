b91a335744257fdefde8560861b52b2d406bf736
CalebNyachwaya <113683930+CalebNyachwaya@users.noreply.github.com>
2024-01-18 19:23:15 +0300

    Add changes

---
+++ test.js
@@ -0,0 +1,9 @@
+function showContainer2() {
+  // Get references to the containers
+  var container1 = document.getElementById("console");
+  var container2 = document.getElementById("container2");
+
+  // Hide container 1 and show container 2
+  container1.style.display = "none";
+  container2.style.display = "block";
+}
